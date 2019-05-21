3913ad8cd3d95bed34ea07d4f6c9f9ad750735cc
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-05-21 13:29:16 +0300

    Add error handling to Chuck.

--- chuck-norris-example/src/components/jokes.js
+++ chuck-norris-example/src/components/jokes.js
@@ -2,27 +2,39 @@ import * as api from "../api";
 import * as React from "react";
 import { Box, Heading, Paragraph } from "grommet";
 
-const Jokes = props => {
+const Jokes = ({ amount }) => {
+  const [error, setError] = React.useState(undefined);
   const [loading, setIsLoading] = React.useState(false);
   const [jokes, setJokes] = React.useState([]);
 
   React.useEffect(() => {
     async function updateJokes() {
       setIsLoading(true);
-      const newJokes = await Promise.all(api.getRandomJokes(props.amount));
-      setIsLoading(false);
-      setJokes(newJokes);
+      try {
+        const newJokes = await Promise.all(api.getRandomJokes(amount));
+        setJokes(newJokes);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setIsLoading(false);
+      }
     }
     updateJokes();
-  }, [props.amount]);
+  }, [amount]);
 
   return (
-    <Box direction="column" justify="start">
+    <Box direction="column" align="center" justify="start">
       <Heading level="3" alignSelf="center">
         Your daily Chuck dose
       </Heading>
-      {loading && <Paragraph>Loading...</Paragraph>}
-      {!loading &&
+      {error && (
+        <Paragraph>
+          Sorry, failed fetching Chuck facts. Please try again soon.
+        </Paragraph>
+      )}
+      {!error && loading && <Paragraph>Loading...</Paragraph>}
+      {!error &&
+        !loading &&
         jokes.map((joke, i) => <Paragraph key={`joke_${i}`}>{joke}</Paragraph>)}
     </Box>
   );
