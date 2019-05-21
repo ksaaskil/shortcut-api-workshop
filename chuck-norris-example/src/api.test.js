444d3d7dd23b20861f9c1a597db4e21bb513fd75
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-05-21 14:01:27 +0300

    Add tests with mock axios.

---
+++ chuck-norris-example/src/api.test.js
@@ -0,0 +1,33 @@
+import * as api from "./api";
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
+
+describe("Joke API with mock axios", () => {
+  const joke = "Chuck says &amp; does.";
+  beforeAll(() => {
+    const mockAxios = new MockAdapter(axios);
+    mockAxios.onGet(/jokes\/random/).reply(200, {
+      value: { joke },
+    });
+  });
+
+  it("creates an empty array of given length", async () => {
+    const array = api.getArrayOfLengthN(5);
+    expect(array).toHaveLength(5);
+  });
+
+  it("fetches unescaped jokes in imperative fashion", async () => {
+    const joke = await api.getRandomJoke();
+    expect(joke).toEqual(`Chuck says & does.`);
+  });
+
+  it("fetches unescaped joke in functional fashion", async () => {
+    const joke = await api.getRandomJokeFp();
+    expect(joke).toEqual(`Chuck says & does.`);
+  });
+
+  it("fetches many jokes", async () => {
+    const jokes = await Promise.all(api.getRandomJokes(3));
+    expect(jokes).toHaveLength(3);
+  });
+});
